ed943dc41ad27555e8386ebaafce54b46cc85c19
JisongKim <2021112431@dgu.ac.kr>
2024-07-29 15:00:39 +0900

    App 수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React,{useEffect, useState} from 'react';
-import './App.css';
-import Map from './Map/Map';
+// import React,{useEffect, useState} from 'react';
+// import './App.css';
+// import Map from './components/Map/Map';
+// import Header from './components/Header/Header';
+// import Footer from './components/Footer/Footer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Map />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
-function App() {
-
-  return (
-    
-      <Map>
-      
-      </Map>
-    
-    
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                {/* 다른 페이지 라우트를 여기에 추가합니다. */}
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
