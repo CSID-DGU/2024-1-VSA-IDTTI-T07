2886e4210394cbb12338e21d420c2b4ee3dbc323
JisongKim <2021112431@dgu.ac.kr>
2024-08-01 14:35:56 +0900

    feat: 하단 바 버튼 추가

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
-// import './Footer.css';
+import { Link } from 'react-router-dom';
+import homeIcon from '../../assets/images/home.svg';
+import frequentIcon from '../../assets/images/frequent.svg';
+import locationIcon from '../../assets/images/location.svg';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; ㅇㅇㅇ </p>
+            <Link to="/" className="footer-button">
+                <img src={homeIcon} alt="Home" />
+            </Link>
+            <Link to="/frequent" className="footer-button">
+                <img src={frequentIcon} alt="Frequent" />
+            </Link>
+            <Link to="/location" className="footer-button">
+                <img src={locationIcon} alt="Location" />
+            </Link>
         </footer>
     );
 };
