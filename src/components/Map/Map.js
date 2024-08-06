3bbaba4f1156e254156fb57201439a9a71db307e
LJongJoo <tlzhsh2@dgu.ac.kr>
2024-08-06 13:47:01 +0900

    feat:지도 확대 축소 버튼 추가

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -16,6 +16,9 @@ const Map = () => {
         };
 
         var map = new kakao.maps.Map(container, options);
+        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+        var zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
         setMap(map);
     }, []);
 
