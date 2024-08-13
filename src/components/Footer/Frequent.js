eb0ac5681b0addf7e7e5734123049c187cdd8712
LJongJoo <tlzhsh2@dgu.ac.kr>
2024-08-13 12:52:04 +0900

    fix : DB에 계속 요청 보내는 버그 수정

--- src/components/Footer/Frequent.js
+++ src/components/Footer/Frequent.js
@@ -6,17 +6,19 @@ const Frequent = () => {
     const { getFavoriteParkingData } = useFavoriteManager();
     const [favoriteParkings, setFavoriteParkings] = useState([]);
 
+    const fetchFavoriteParkings = async () => {
+        const data = await getFavoriteParkingData();
+        setFavoriteParkings(data);
+    };
+
     useEffect(() => {
-        const fetchFavoriteParkings = async () => {
-            const data = await getFavoriteParkingData();
-            setFavoriteParkings(data);
-        };
-        fetchFavoriteParkings();
-    }, [getFavoriteParkingData]);
+        fetchFavoriteParkings(); // 컴포넌트가 처음 로드될 때 한 번 호출
+    }, []);
 
     return (
         <div className="frequent-container">
             <h2>주차장 즐겨찾기 목록</h2>
+            <button onClick={fetchFavoriteParkings}>즐겨찾기 목록 새로고침</button>
             <ul className="frequent-list">
                 {favoriteParkings.map(parking => (
                     <li key={parking.code}>
