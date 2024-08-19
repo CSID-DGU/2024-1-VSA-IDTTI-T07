8b790ab85c60b208790e4523b4a15337d149ba6b
LJongJoo <tlzhsh2@dgu.ac.kr>
2024-08-19 14:00:35 +0900

    chore : location 관련 css 재설정

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../App.css';
+// import '../App.css';
 import Footer from '../components/Footer/Footer';
 import Frequent from '../components/Footer/Frequent';
 import { usePrediction } from '../context/PredictionContext';
 import { useNavigate } from 'react-router-dom';
 import LocationMap from '../components/Location/LocationMapCom'; // LocationMap 컴포넌트 불러오기
-import Map from '../components/Map/Map'
 import Header from '../components/Header/Header';
+import './Location.css'; // 새로 만든 CSS 파일 불러오기
 
 const Location = () => {
     const { setPrediction } = usePrediction();
@@ -55,64 +55,58 @@ const Location = () => {
     return (
         <div className="App">
             <Header/>
-            <h1>Location Page</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>도착지:</label>
-                    <input 
-                        type="text" 
-                        value={destination} 
-                        onChange={(e) => setDestination(e.target.value)} 
-                        placeholder="도착지를 입력하세요" 
-                    />
-                </div>
-                <div>
-                    <label>도착 시간:</label>
-                    <select 
-                        value={meridiem} 
-                        onChange={(e) => setMeridiem(e.target.value)}
-                    >
-                        <option value="AM">오전</option>
-                        <option value="PM">오후</option>
-                    </select>
-                    <input 
-                        type="number" 
-                        value={hour} 
-                        onChange={(e) => setHour(e.target.value)} 
-                        placeholder="시" 
-                        min="1" 
-                        max="12"
-                    />
-                    <span>:</span>
-                    <input 
-                        type="number" 
-                        value={minute} 
-                        onChange={(e) => setMinute(e.target.value)} 
-                        placeholder="분" 
-                        min="0" 
-                        max="59"
-                    />
-                </div>
-                <div>
-                    <label>요일:</label>
-                    <select 
-                        value={weekday} 
-                        onChange={(e) => setWeekday(e.target.value)}
-                    >
-                        <option value="Sunday">일요일</option>
-                        <option value="Monday">월요일</option>
-                        <option value="Tuesday">화요일</option>
-                        <option value="Wednesday">수요일</option>
-                        <option value="Thursday">목요일</option>
-                        <option value="Friday">금요일</option>
-                        <option value="Saturday">토요일</option>
-                    </select>
-                </div>
-                <button type="submit">설정</button>
-            </form>
+            <div className="location-container">
+                <form className="location-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label>도착 시간:</label>
+                        <select 
+                            value={meridiem} 
+                            onChange={(e) => setMeridiem(e.target.value)}
+                        >
+                            <option value="AM">오전</option>
+                            <option value="PM">오후</option>
+                        </select>
+                        <input 
+                            type="number" 
+                            value={hour} 
+                            onChange={(e) => setHour(e.target.value)} 
+                            placeholder="시" 
+                            min="1" 
+                            max="12"
+                        />
+                        <span>:</span>
+                        <input 
+                            type="number" 
+                            value={minute} 
+                            onChange={(e) => setMinute(e.target.value)} 
+                            placeholder="분" 
+                            min="0" 
+                            max="59"
+                        />
+                    </div>
+                    <div>
+                        <label>요일:</label>
+                        <select 
+                            value={weekday} 
+                            onChange={(e) => setWeekday(e.target.value)}
+                        >
+                            <option value="Sunday">일요일</option>
+                            <option value="Monday">월요일</option>
+                            <option value="Tuesday">화요일</option>
+                            <option value="Wednesday">수요일</option>
+                            <option value="Thursday">목요일</option>
+                            <option value="Friday">금요일</option>
+                            <option value="Saturday">토요일</option>
+                        </select>
+                    </div>
+                    <button type="submit">설정</button>
+                </form>
 
-            {/* 지도 컴포넌트 삽입 */}
-            <LocationMap />
+                {/* 지도 컴포넌트 삽입 */}
+                <div className="location-map">
+                    <LocationMap />
+                </div>
+            </div>
 
             <Footer toggleAccordion={toggleAccordion} />
             <div className={`accordion ${isAccordionOpen ? 'open' : ''}`}>
